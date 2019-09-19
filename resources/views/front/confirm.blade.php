b94f37f31a16dc6468e00be0b08f1ac5be747a98
suphakorn <gan.suphakorn17@gmail.com>
2019-09-19 18:00:08 +0900

    confirmに「再入力」ボタン修正

--- resources/views/front/confirm.blade.php
+++ resources/views/front/confirm.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('front.layouts.app')
 @section('title','内容確認')
 @section('content')
 
@@ -7,7 +7,6 @@
   <div class="row">
     <div class="col-md-12"><br />
     <h3 align="center">内容確認</h3><br />
-
       <table class="table">
         <tr>
           <td style="width:25%">名前</td>
@@ -65,12 +64,6 @@
 
     <div class="form-group" align="center">
 
-      {{-- 入力画面に戻る --}}
-      <form method="post" action="{{route('index')}}" enctype="multipart/form-data">
-        <button type="submit" name="action" value="post" class="btn btn-primary">再入力</button>
-      </form>
-
-      {{-- アンケート保存する --}}
       <form method="post" action="{{URL::previous()}}" enctype="multipart/form-data">
         {{csrf_field()}}
         <input type="hidden" name="fullname" class="form-control" value="{{ $fullname }}">
@@ -80,6 +73,10 @@
         <input type="hidden" name="is_send_email" class="form-control" value="{{ $is_send_email }}">
         <input type="hidden" name="feedback" class="form-control" value="{!! nl2br(htmlspecialchars($feedback, ENT_QUOTES, 'UTF-8', false), false) !!}">
 
+        {{-- 入力画面に戻る --}}
+        <a href="javascript:history.back();" class="btn btn-primary">再入力</a>
+
+        {{-- アンケート保存する --}}
         <button type="submit" name="action" value="back" class="btn btn-success">送信</button>
       </form>
 
